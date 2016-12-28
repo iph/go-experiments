6a41e1cafa096bcb257a84db4dbf7d22da3b671d
Sean Tyler Myers <seamyers@amazon.com>
2016-12-28 13:46:43 -0800

    Added const (3.13) exercise

---
+++ book/3/consts/main.go
@@ -0,0 +1,17 @@
+package main
+
+const (
+	// Multiplier - In case we want to change bases
+	Multiplier = 1000
+	KB         = Multiplier
+	MB         = KB * Multiplier
+	GB         = MB * Multiplier
+	TB         = GB * Multiplier
+	PB         = TB * Multiplier
+	EB         = PB * Multiplier
+	YB         = EB * Multiplier
+)
+
+func main() {
+
+}
